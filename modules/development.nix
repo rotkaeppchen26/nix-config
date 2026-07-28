56c09e5bdb43b3efa6996917593ebddae9420785
rotkaeppchen26 <schoefl.josef@gmail.com>
2026-07-28 23:14:32 +0200

    nixos-rebuild: updated lenny

--- modules/development.nix
+++ modules/development.nix
@@ -15,9 +15,18 @@
 
   ];
 
-  # Enable Docker daemon
-  virtualisation.docker.enable = true;
+  # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
 
-  # Add your user to the docker group
-  users.users.${username}.extraGroups = [ "docker" ];
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
 }
