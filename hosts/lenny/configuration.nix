40d87009fe29ada6cb3414654aa08fb43fcb247a
rotkaeppchen26 <schoefl.josef@gmail.com>
2026-07-28 23:01:32 +0200

    nixos-rebuild: updated lenny

--- hosts/lenny/configuration.nix
+++ hosts/lenny/configuration.nix
@@ -143,12 +143,27 @@
     libusbsio
     pkg-config
     glew glfw
+    glibc
+    zlib
+    libgcc
+    stdenv.cc.cc
+    libGL
+    libGLU
     libx11
     libxrandr
     libxcursor
     libxi
     libxinerama
     alsa-lib pulseaudio
+    libudev-zero
+    ffmpeg_4
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
+    libva
   ];
 
   # List packages installed in system profile. To search, run:
