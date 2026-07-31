a3ed36e45bae6ecbd41bab1924abe2ef3b9f1653
rotkaeppchen26 <schoefl.josef@gmail.com>
2026-07-31 06:45:31 +0200

    add 7z; clean comments

--- hosts/lenny/configuration.nix
+++ hosts/lenny/configuration.nix
@@ -135,41 +135,11 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # # Allow execution of random binaries
-  # programs.nix-ld.enable = true;
-  # programs.nix-ld.libraries = with pkgs; [
-  #   # Any missing dyn libs for unpackaged programs here, NOT in environment.systemPackagres
-  #   libusb1
-  #   libusbsio
-  #   pkg-config
-  #   glew glfw
-  #   glibc
-  #   zlib
-  #   libgcc
-  #   stdenv.cc.cc
-  #   libGL
-  #   libGLU
-  #   libx11
-  #   libxrandr
-  #   libxcursor
-  #   libxi
-  #   libxinerama
-  #   alsa-lib pulseaudio
-  #   libudev-zero
-  #   ffmpeg_4
-  #   gst_all_1.gstreamer
-  #   gst_all_1.gst-plugins-base
-  #   gst_all_1.gst-plugins-good
-  #   gst_all_1.gst-plugins-bad
-  #   gst_all_1.gst-plugins-ugly
-  #   gst_all_1.gst-libav
-  #   libva
-  # ];
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     lshw # hardware listener
+    p7zip
 
     figlet lolcat cowsay
     discord qpwgraph discord-canary
@@ -177,18 +147,6 @@
 
     moonlight-qt
   ];
-
-  # fonts = {
-  #   packages = with pkgs; [
-  #     font-awesome
-  #   ];
-  #   fontconfig = {
-  #     enable = true;
-  #     defaultFonts = {
-  #       monospace = [ "FiraCode Nerd Font" "Font Awesome 6 Free" ];
-  #     };
-  #   };
-  # };
 
   nixpkgs.config.permittedInsecurePackages = [
     "openssl-1.1.1w"
